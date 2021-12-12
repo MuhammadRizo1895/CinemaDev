e93dae373c4c19c419535df363443f64cc0c6362
unknown <muhammadrizoyolbarsbekov.com>
2021-12-13 00:00:44 +0500

    firts commit

---
+++ main.js
@@ -0,0 +1,275 @@
+var cinema = [
+   {
+       "id":1,
+       "name": "Finch",
+       "img": "https://resizer.mail.ru/p/d16d2b62-7a9d-5779-a1b0-c4f1fd181cc9/AQACIYMM-4RgVjrpHWOzWT_VfTHstzc8zMQJ8FzcihEnbfgij_0m_5gp8vT4w5CPZep4OieQKJqC9to8mn7pPm1ALAk.jpg",
+       "title": "Sarguzasht",
+       "yid": "https://www.youtube.com/results?search_query=finch+trailer",
+       janr:["trailer" , " fantastik"]
+   },
+   {
+    "id":2,
+    "name": "Kill Bill",
+    "img": "https://content.rozetka.com.ua/goods/images/big/23632638.jpg",
+    "title": "Qasos kuni",
+    "yid": "https://www.youtube.com/watch?v=-q7EiQLh_ls",
+    janr:["trailer", "jangari"]
+   },
+   {
+    "id":3,
+    "name": "Birinchi qor",
+    "img": "https://upload.wikimedia.org/wikipedia/en/thumb/5/50/Virgin_Snow_%28film%29.jpg/220px-Virgin_Snow_%28film%29.jpg",
+    "title": "Muhabat qissasi",
+    "yid": "https://www.youtube.com/watch?v=JRY30MruDLk&list=PL22CA2A010F146088",
+    janr:["romantic", "melodramma"]
+   },
+   {
+    "id":4,
+    "name": "Valine'mat",
+    "img": "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQznNgS8YTI1e3bYCPIuzxWQjEx6PSY2_3jsvulVqMF5oPSNRZu8IyioMRDbxp1MIss4X8&usqp=CAU",
+    "title": "Mafiya sardori",
+    "yid": "https://www.youtube.com/watch?v=vOrabea_7ik",
+    janr:["trailer", "jangari"]
+   },
+   {
+    "id":5,
+    "name": "Qasoskorlar",
+    "img": "https://i.mycdn.me/image?id=880477965707&t=50&plc=WEB&tkn=*qQtyQ3kGYpmRSTXrYsxhgI10KC0&fn=external_7",
+    "title": "Chekiszlik jangi",
+    "yid": "https://www.youtube.com/watch?v=IjeaL5Fo-Lo",
+    janr:[ "jangari", "fantastik"]
+   },
+   {
+    "id":6,
+    "name": "Uddaburon yigitlar 3",
+    "img": "http://asil-media.net/rasmlar/images/2020/04/03/e9864027aab5bed21c054112f835e47e.jpg",
+    "title": "Hamisha birga",
+    "yid": "https://www.youtube.com/watch?v=0V4AUjt_VWU",
+    janr:["sarguzasht", "jangari"]
+   },
+   {
+    "id":7,
+    "name": "Avatar",
+    "img": "https://m.media-amazon.com/images/I/816iHEvF7-L._AC_SY679_.jpg",
+    "title": "Hamisha birga",
+    "yid": "https://www.youtube.com/watch?v=3FSjNmuVa7A",
+    janr:[ "jangari", "fantastik"]
+   },
+   {
+    "id":8,
+    "name": "Suyunchi",
+    "img": "https://sirensong.ru/uploads/thumbs/0/2/9/029f9d2b7660633d072738ad2d1b64bd.jpg",
+    "title": "Milliy kino",
+    "yid": "https://www.youtube.com/watch?v=fJbCVxFBMiU",
+    janr:["kamediya",]
+   },
+   {
+    "id":9,
+    "name": "Shum bola",
+    "img": "https://i.ytimg.com/vi/5Qk8RXrIgKc/maxresdefault.jpg",
+    "title": "Milliy kino",
+    "yid": "https://www.youtube.com/watch?v=IN_YECwjY6g",
+    janr:["kamediya","sarguzasht"]
+   },
+   {
+    "id":10,
+    "name": "Terminator",
+    "img": "https://avatars.mds.yandex.net/get-kinopoisk-image/1600647/537869cd-5fb8-4993-af4c-c5f6a30e61ba/600x900",
+    "title": "1984",
+    "yid": "https://www.youtube.com/watch?v=iHD0JXpXKTQ",
+    janr:["trailer", "fantastik"]
+   },
+   {
+    "id":11,
+    "name": "Terminator 2",
+    "img": "https://kinohit.uz/uploads/posts/2019-03/1552204211_kinopoisk_ru-terminator-2_3a-judgment-day-2300671.jpg",
+    "title": "Hukm kuni",
+    "yid": "https://www.youtube.com/watch?v=mrDtfnZf2zs",
+    janr:["trailer", "fantastik"]
+   },
+   {
+    "id":12,
+    "name": "Terminator 3",
+    "img": "https://www.1zoom.me/big2/20/111527-dender.jpg",
+    "title": "Mashinalar yuksalishi",
+    "yid": "https://www.youtube.com/watch?v=ncUq-BER984",
+    janr:["trailer", "fantastik"]
+   },
+   {
+    "id":13,
+    "name": "Yengilmas qotil",
+    "img": "https://i.ytimg.com/vi/mgkpH5zfIhs/mqdefault.jpg",
+    "title": "Qotilning qasosi",
+    "yid": "https://www.youtube.com/watch?v=mgkpH5zfIhs",
+    janr:["trailer","sarguzasht"]
+   },
+   {
+    "id":14,
+    "name": "Mo'jiza",
+    "img": "http://www.tarona.tv/_bd/28/04703768.jpg",
+    "title": "Ishonch haqida film",
+    "yid": "https://www.youtube.com/watch?v=WGCoCv_ASiI",
+    janr:["trailer","sarguzasht"]
+   },
+   {
+    "id":15,
+    "name": "O'rgimchak odam",
+    "img": "https://www.kino-teatr.ru/movie/posters/big/4/99954.jpg",
+    "title": "Yangi haloskor",
+    "yid": "https://www.youtube.com/watch?v=_xXLWjhp6jc",
+    janr:["trailer","sarguzasht"]
+   },
+   {
+    "id":16,
+    "name": "O'rgimchak odam",
+    "img": "https://cdn.vox-cdn.com/thumbor/nUHZ7gpoF4VXhS99duZPfV9VpEQ=/0x0:600x750/1200x800/filters:focal(163x213:259x309)/cdn.vox-cdn.com/uploads/chorus_image/image/70152383/unnamed__1_.0.jpeg",
+    "title": "Uyga yo'l yo'q",
+    "yid": "https://www.youtube.com/watch?v=_33mjmM0wTU",
+    janr:["trailer","sarguzasht"]
+   },
+   {
+    "id":17,
+    "name": "Muhabbat oyatlari",
+    "img": "https://lh3.googleusercontent.com/proxy/-guifaSsBagtafoobbnLZAeCjQYwsnBEhatMyGATeYGIr0rjxEc86PWWyOHyAQbF6uF5EqWhSEs0kRo",
+    "title": "Sevgida toqat",
+    "yid": "https://www.youtube.com/watch?v=CuT5I63mtfE",
+    janr:["diniy", "muhabbat"]
+   },
+   {
+    "id":18,
+    "name": "Muhabbat oyatlari 2",
+    "img": "https://uzstudio.tv/uploads/posts/2021-02/medium/1613555062_image_1.jpg",
+    "title": "Sabrning mukofoti",
+    "yid": "https://www.youtube.com/watch?v=Oy323uj6xRE",
+    janr:["diniy", "muhabbat"]
+   },
+   {
+    "id":19,
+    "name": "Kung-fu panda",
+    "img": "https://kinohit.uz/uploads/posts/2019-10/1570374172_mv5bodjkztzhmwitmdi3yy00zwzlltk4njqtoti1zju5njbjztvjxkeyxkfqcgdeqxvyode5nze3ote__v1_sy1000_cr006891000_al_.jpg",
+    "title": "Ajdarho jangchisi",
+    "yid": "https://www.youtube.com/watch?v=lqXXPNISueo",
+    janr:["sarguzasht","comedic"]
+   },
+   {
+    "id":20,
+    "name": "Ipman 1",
+    "img": "https://m.media-amazon.com/images/M/MV5BNTFmMjM3M2UtOTIyZC00Zjk3LTkzODUtYTdhNGRmNzFhYzcyXkEyXkFqcGdeQXVyMTMxODk2OTU@._V1_.jpg",
+    "title": "Og'ir davr",
+    "yid": "https://www.youtube.com/watch?v=1oBEHUQD1T4",
+    janr:["trailer","tarixiy"]
+   },
+   {
+    "id":21,
+    "name": "Ipman 2",
+    "img": "https://m.media-amazon.com/images/M/MV5BZWM2MTcyMDgtZTZkNS00NTg5LWIwNzEtMmY1Y2I1MTZjYTE0XkEyXkFqcGdeQXVyMTMxODk2OTU@._V1_.jpg",
+    "title": "Buyuk ustoz afsonasi",
+    "yid": "https://www.youtube.com/watch?v=xQ8RljOKoOg",
+    janr:["trailer","tarixiy"]
+   },
+   {
+    "id":22,
+    "name": "Novda",
+    "img": "https://tasix.online/uploads/posts/2020-10/1603779365_unnamed.jpg",
+    "title": "Beg'ubor bolalik",
+    "yid": "https://www.youtube.com/watch?v=FVSAtbNsJzI",
+    janr:["comedic"]
+   },
+   {
+    "id":23,
+    "name": "Vatan",
+    "img": "http://baxrom.ucoz.com/_nw/23/86222999.jpg",
+    "title": "Ona yurt, qasos",
+    "yid": "https://www.youtube.com/watch?v=sKY6FoWnaek",
+    janr:["comedic", "melodramma"]
+   },
+   {
+    "id":24,
+    "name": "Osmondagi bolalar ",
+    "img": "https://i.ytimg.com/vi/R0k-sQlqL8c/maxresdefault.jpg",
+    "title": "Milliy kino",
+    "yid": "https://www.youtube.com/watch?v=R0k-sQlqL8c",
+    janr:["comedic", "melodramma"]
+   },
+   {
+    "id":25,
+    "name": "Osmondagi bolalar-2 ",
+    "img": "https://i.ytimg.com/vi/LDe_J4r5V1k/maxresdefault.jpg",
+    "title": "Milliy kino",
+    "yid": "https://www.youtube.com/results?search_query=osmondagi+bolalar+2",
+    janr:["comedic", "melodramma"]
+   },
+   {
+    "id":26,
+    "name": "Ishdagi ishq ",
+    "img": "http://asil-media.net/uploads/mini/schema/c0/eb21f42e8c6a847526105b2f2dce63.jpg",
+    "title": "Ishq va ish ",
+    "yid": "https://www.youtube.com/watch?v=mtNhiwdTctU",
+    janr:["comedic","romantic",]
+   },
+   {
+    "id":27,
+    "name": "Devdas ",
+    "img": "http://asil-media.net/uploads/mini/fullstory/40/672b02a844346c0cc42a348d2f0921.jpg",
+    "title": "Hind kinosi ",
+    "yid": "https://www.youtube.com/watch?v=dlW51CDKFco",
+    janr:["melodramma","romantic",]
+   },
+   {
+    "id":28,
+    "name": "O'g'irlanmagan kelin ",
+    "img": "http://asil-media.net/uploads/mini/fullstory/f5/8dee9c6bfc0687b55a75dc8b30f76d.jpg",
+    "title": "Hind kinosi ",
+    "yid": "https://www.youtube.com/watch?v=W4cF0bFT6HM",
+    janr:["melodramma","romantic",]
+   },
+   {
+    "id":29,
+    "name": "Dengizchi papay ",
+    "img": "https://lh3.googleusercontent.com/proxy/1gveAadmS9G3EMr-sInj8qV-4vibsIMuSNjBHgymiKSiWVXtAaIzKLlPqZTL8Qr68NcmPXsrr2wNPEYq0bH8iHr-d5SNZsys",
+    "title": "Multfilm ",
+    "yid": "https://www.youtube.com/watch?v=RKfRE4AlW7s",
+    janr:["comedic"]
+   },
+   {
+    "id":30,
+    "name": "Men kimman",
+    "img": "https://lh3.googleusercontent.com/proxy/I9LSY5h67VAwI0Z1Dy4_4nePDM1hJBk3BPZxmec5y7j2IZkJQzOpAkQH2-J2bEWDSqpnS21MZNaduxSDpsMKVg",
+    "title": "Jangari film",
+    "yid": "https://www.youtube.com/watch?v=GfkkOa4ZMeg",
+    janr:["comedic", "trailer"]
+   },
+]
+var elrow = document.querySelector('.container__item');
+let content = '<div class="col-2 py-3 m-3" ';
+for (let i=0; i<cinema.length; i++){
+
+    content +='<div class="col-2 py-3 m-3" >' + '<a href= " '+cinema[i].yid +' ">' + '<img src= " '+ cinema[i].img +' "> '+'<span>'+'<i class="fas fa-play">'+'</i>'+'</span>' + '<p>' + cinema[i].name +  '</p>'+ '</a>'  + '<u>' + cinema[i].title + '</u>' + '<i class="fas fa-thumbs-up">' + '</i>' + '<br>' + '<small>'+"Janri: "+cinema[i].janr+'</small>' + '<i class="fas fa-download">'+'</i>' +  '<i class="fas fa-heart">'+'</i>'+  '</div>'
+}
+content +='</div>';
+
+elrow.innerHTML = content;
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// var elrow = document.quarySelector('row')
+// var elcol = document.querySelector('.col-2');
+// var elcol = "<div>"
+// for (let i=0; i < cinema.length; i++){
+//     elrow += '<a href= "'+ cinema[i].yid +' " >' +'<img src="' +cinema[i].img + '">' + '</a>'
+
+// }
+// elrow+="</div>";
+// elcol.innerHTML = elrow;
